1613ef26f6eca44f6eeaaa7ce5b4cfea20f8813a
ValeriaAniseedr <100408314+ValeriaAniseedr@users.noreply.github.com>
2022-03-12 01:31:22 +0700

    Add files via upload

--- morze.c
+++ morze.c
@@ -9,29 +9,23 @@ typedef struct {
 
 int main() {
     FILE *file;
-    morze alf[26];
+    morze alf[27];
     int i=0;
-    file = fopen("morze.txt", "r");
-    char kostil[2];
-    char str[] = "**** * *-** *-** ---  *-- --- *-* *-** -**";
-
-    while (fscanf(file, "%c%s%c", &(alf[i].c), alf[i].m, kostil) != EOF) {
-        printf("%c %s", alf[i].c, alf[i].m);
+    file = fopen("morze.txt", "r");   
+    while (fscanf(file, "%c%s%*c", &(alf[i].c), alf[i].m) != EOF)
         i++;
-    }
     fclose(file);
-
+    char str[]="";
+    gets(str);
     char *pch = strtok (str, " ");
     while (pch != NULL) {
       for (int i=0; i<26;i++){
-        if (alf[i].m == pch)
-      printf("%c \n", alf[i].c);
+        if (strcmp(pch, alf[i].m) == 0)
+          printf("%c", alf[i].c);
       }
-      printf("\n%s", pch);
       pch = strtok (NULL, " ");
     }
-    pch = "****";
-    printf("\n%s %s %d\n", pch, alf[7].m, pch == alf[7].m);
+    printf("\n");
   return 0;
 }
 
