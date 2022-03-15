84265342e573777b629c3e432045bb7f1495b513
Lera <valeria.yuryevna@gmail.com>
2022-03-15 15:17:17 +0700

    15.03

--- morze.c
+++ morze.c
@@ -11,12 +11,15 @@ int main() {
     FILE *file;
     morze alf[27];
     int i=0;
-    file = fopen("morze.txt", "r");   
+ 
+    file = fopen("morze2.txt", "r");   
     while (fscanf(file, "%c%s%*c", &(alf[i].c), alf[i].m) != EOF)
         i++;
+    for (int i=0; i<26;i++)
+      printf("%c%s ",alf[i].c,alf[i].m);
     fclose(file);
-    char str[]="";
-    gets(str);
+    char str[1000]="";
+    fgets(str,10000,stdin);
     char *pch = strtok (str, " ");
     while (pch != NULL) {
       for (int i=0; i<26;i++){
