96ca3e804cd992a6fe3b5207998c0c2549f4bae9
ValeriaAniseedr <valeria.yuryevna@gmail.com>
2022-03-18 16:59:09 +0700

    18/03

--- morze.c
+++ morze.c
@@ -11,22 +11,34 @@ int main() {
     FILE *file;
     morze alf[27];
     int i=0;
- 
+   
+
     file = fopen("morze_linux.txt", "r");   
     while (fscanf(file, "%c%s%*c", &(alf[i].c), alf[i].m) != EOF)
         i++;
-    for (int i=0; i<26;i++)
-      printf("%c%s ",alf[i].c,alf[i].m);
+   // for (int i=0; i<26;i++)
+    //  printf("%c%s ",alf[i].c,alf[i].m);
     fclose(file);
-    char str[1000]="";
-    fgets(str,10000,stdin);
-    char *pch = strtok (str, " ");
-    while (pch != NULL) {
+    
+    char ch;
+    while ((ch = getchar()) != EOF) {
+      char symbol[5];
+      for (int i = 0; i < 5; i++) {
+        if (ch == ' ') {
+            symbol[i] = '\0';
+          // printf(" ");
+            break;
+         } else {
+            symbol[i]  = ch;
+            ch = getchar();
+          }
+      }
       for (int i=0; i<26;i++){
-        if (strcmp(pch, alf[i].m) == 0)
+        if (strcmp(symbol, alf[i].m) == 0)
           printf("%c", alf[i].c);
-      }              // Hi Hi
-      pch = strtok (NULL, " ");
+      }       
+      if (symbol[0]=='\0')
+        printf("!");
     }
     printf("\n");
   return 0;
